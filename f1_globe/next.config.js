0aeb9e20aa984781ca0db9925010f3184f08dbc3
Vincent Allport <vincentallport@gmail.com>
2026-04-01 08:27:54 -0400

    Updating config for Netlify deployment.

--- f1_globe/next.config.js
+++ f1_globe/next.config.js
@@ -1,7 +1,6 @@
 const nextConfig = {
   output: 'export',
   basePath: '/f1_globe',       // Ensures routing is relative to /f1_globe
-  assetPrefix: '/f1_globe',    // Ensures static assets are loaded correctly
   trailingSlash: true,         // Ensures consistent trailing slashes for paths (optional)
 };
 
